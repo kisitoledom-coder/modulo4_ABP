d67dd4bb1ff3b1c04fd0b0b03aee1797894d15bd
kisi toledo <kitoledo@egresados.ubiobio.cl>
2026-07-10 21:35:16 -0400

    Lección 1 completada: Uso de console.log, prompt y alert

---
+++ script.js
@@ -0,0 +1,5 @@
+// Mostrar mensaje en la consola
+console.log("¡Aplicación de consola iniciada correctamente!");
+
+// Recibir datos del usuario con prompt() y mostrar un mensaje personalizado con alert()
+alert("¡Hola, " + prompt("Por favor, ingresa tu nombre para comenzar:") + "!");
